490b070d7528124e25243b5b94d938dedbcaada8
jardahrazdera <jarek@jaroslav.tech>
2025-12-25 18:24:52 +0100

    fix(backend): allow production domain in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,10 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://astro.jaroslav.tech"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
